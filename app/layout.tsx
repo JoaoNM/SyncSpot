0cdc25180cd8690b50b44e08d239ff3692e6daa7
Joao Nina Matos <51080629+JoaoNM@users.noreply.github.com>
2024-07-04 18:44:15 +0100

    adding fonts + some formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,20 +9,30 @@ import { ReactNode } from "react";
 const font = Work_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SyncSpot | Vercel Next.JS Firebase Shadcn/ui Tailwind Boilerplate",
-  description:
-    "SyncSpot is a Vercel Next.JS Firebase Shadcn/ui Tailwind Boilerplate project to help you get started with your next project.",
+	title: "SyncSpot | Vercel Next.JS Firebase Shadcn/ui Tailwind Boilerplate",
+	description:
+		"SyncSpot is a Vercel Next.JS Firebase Shadcn/ui Tailwind Boilerplate project to help you get started with your next project.",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en">
-      <body className={cn(font.className)}>
-        <MyFirebaseProvider>
-          {children}
-          <Toaster />
-        </MyFirebaseProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap"
+					rel="stylesheet"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;500;700&display=swap"
+					rel="stylesheet"
+				/>
+			</head>
+			<body className={cn(font.className)}>
+				<MyFirebaseProvider>
+					{children}
+					<Toaster />
+				</MyFirebaseProvider>
+			</body>
+		</html>
+	);
 }
