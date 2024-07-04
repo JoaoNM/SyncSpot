72d18e9627778e8b8779d4b6a7b5b9906fd044f6
Joao Nina Matos <51080629+JoaoNM@users.noreply.github.com>
2024-07-04 16:14:04 +0100

    fix team selector

--- components/TeamSelector.tsx
+++ components/TeamSelector.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { TeamType } from "@/store/teamsAtom";
+import { TeamType, teamsAtom } from "@/store/teamsAtom";
 import { selectedTeamAtom } from "@/store/selectedTeamAtom";
 import { useAtom } from "jotai";
 
@@ -8,24 +8,26 @@ interface TeamSelectorProps {
 	setSelectedTeam: (team: TeamType) => void;
 }
 
-const TeamSelector: React.FC<TeamSelectorProps> = ({
-	teams,
-	setSelectedTeam,
-}) => {
+const TeamSelector: React.FC<TeamSelectorProps> = () => {
+	const [selectedTeam, setSelectedTeam] = useAtom(selectedTeamAtom);
+	const [teams] = useAtom(teamsAtom);
 	const [activeTeam, setActiveTeam] = useState<string | null>(null);
 
+	console.log(selectedTeam);
+
 	const handleTeamClick = (team: TeamType) => {
-		setActiveTeam(team.teamId);
+		console.log(team.teamId);
 		setSelectedTeam(team);
 	};
 
 	return (
 		<div className="flex overflow-x-auto whitespace-nowrap p-4 bg-gray-100">
-			{teams.map((team) => (
+			<h1>{selectedTeam && selectedTeam.name}</h1>
+			{teams.map((team: TeamType) => (
 				<div
 					key={team.teamId}
 					className={`inline-block mr-4 px-4 py-2 rounded-lg cursor-pointer transition-colors duration-300 ${
-						activeTeam === team.teamId
+						selectedTeam && selectedTeam.teamId === team.teamId
 							? "bg-blue-500 text-white"
 							: "bg-gray-200"
 					}`}
