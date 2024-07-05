483bfadb508b87cc0ab431e06ee83985f60f0664
Joao Nina Matos <51080629+JoaoNM@users.noreply.github.com>
2024-07-05 23:19:39 +0100

    formatting & removing links

--- components/navbar/navbar-mobile.tsx
+++ components/navbar/navbar-mobile.tsx
@@ -3,50 +3,47 @@
 import { NavbarUserLinks } from "@/components/navbar/navbar-user-links";
 import { buttonVariants } from "@/components/ui/button";
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
 import { MenuIcon } from "lucide-react";
 
 export const NavbarMobile = () => {
-  return (
-    <>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className="-mr-4">
-              <MenuIcon />
-            </NavigationMenuTrigger>
-            <NavigationMenuContent className="flex flex-col p-1">
-              <NavigationMenuLink
-                href="#1"
-                className={buttonVariants({ variant: "link" })}
-              >
-                Item 1
-              </NavigationMenuLink>
-              <NavigationMenuLink
-                href="#2"
-                className={buttonVariants({ variant: "link" })}
-              >
-                Item 2
-              </NavigationMenuLink>
-              <NavigationMenuLink
-                href="#3"
-                className={buttonVariants({ variant: "link" })}
-              >
-                Item 3
-              </NavigationMenuLink>
-              <div className="flex flex-col mb-0.5">
-                <NavbarUserLinks />
-              </div>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </>
-  );
+	return (
+		<>
+			<NavigationMenu>
+				<NavigationMenuList>
+					<NavigationMenuItem>
+						<NavigationMenuTrigger className="-mr-4">
+							<MenuIcon />
+						</NavigationMenuTrigger>
+						<NavigationMenuContent className="flex flex-col p-1">
+							<NavigationMenuLink
+								className={buttonVariants({ variant: "link" })}
+							>
+								Item 1
+							</NavigationMenuLink>
+							<NavigationMenuLink
+								className={buttonVariants({ variant: "link" })}
+							>
+								Item 2
+							</NavigationMenuLink>
+							<NavigationMenuLink
+								className={buttonVariants({ variant: "link" })}
+							>
+								Item 3
+							</NavigationMenuLink>
+							<div className="flex flex-col mb-0.5">
+								<NavbarUserLinks />
+							</div>
+						</NavigationMenuContent>
+					</NavigationMenuItem>
+				</NavigationMenuList>
+			</NavigationMenu>
+		</>
+	);
 };
