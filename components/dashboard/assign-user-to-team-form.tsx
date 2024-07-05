6f67fef9448ea790bcfcf7c64f7bb99d43c126cc
Joao Nina Matos <51080629+JoaoNM@users.noreply.github.com>
2024-07-05 18:27:52 +0100

    invite user component UI fixed

--- components/dashboard/assign-user-to-team-form.tsx
+++ components/dashboard/assign-user-to-team-form.tsx
@@ -5,43 +5,120 @@ import { useState, useEffect } from "react";
 import { useFirebaseOperations } from "@/lib/firebase-operations";
 import { Input } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
-import { userAtom } from "@/store/authAtom";
+import { selectedTeamAtom } from "@/store/selectedTeamAtom";
 import { useAtom } from "jotai";
+import { toast } from "@/components/ui/use-toast";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
 
 const AssignUserToTeamForm = () => {
-	const [user] = useAtom(userAtom);
+	const [selectedTeam] = useAtom(selectedTeamAtom);
 	const [email, setEmail] = useState("");
-	const [teamId, setTeamId] = useState("");
+	const [isDialogOpen, setIsDialogOpen] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 
 	const { assignUserToTeamByEmail } = useFirebaseOperations();
 
-	const handleSubmit = (e: React.FormEvent) => {
-		e.preventDefault();
-		assignUserToTeamByEmail(email, teamId);
-		setEmail("");
-		setTeamId("");
+	const handleSubmit = async () => {
+		if (selectedTeam && selectedTeam.teamId) {
+			setIsLoading(true);
+			if (email.length > 0) {
+				try {
+					await assignUserToTeamByEmail(email, selectedTeam.teamId);
+					toast({
+						title: "Team Updated!",
+						description: `We've added ${email} (if they have a SyncSpot account)`,
+					});
+					setIsDialogOpen(false);
+				} catch {
+					toast({
+						title: "Error",
+						description: "There was an issue creating the team.",
+						variant: "destructive",
+					});
+				} finally {
+					setIsLoading(false);
+				}
+			} else {
+				setIsLoading(false);
+				toast({
+					title: "Error",
+					description: "You need to enter an email",
+					variant: "destructive",
+				});
+			}
+		} else {
+			toast({
+				title: "Error",
+				description: "No team selected",
+				variant: "destructive",
+			});
+		}
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<div>
-				<label>Email:</label>
-				<Input
-					type="text"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-			</div>
-			<div>
-				<label>Team ID:</label>
-				<Input
-					type="text"
-					value={teamId}
-					onChange={(e) => setTeamId(e.target.value)}
-				/>
-			</div>
-			<Button type="submit">Assign User to Team</Button>
-		</form>
+		<Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+			<DialogTrigger asChild>
+				<Button variant="outline" size="xs">
+					<span>Add Teammate</span>
+				</Button>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-[525px]">
+				<DialogHeader>
+					<DialogTitle>Add Teammate</DialogTitle>
+					<DialogDescription>
+						Add a teammate with their email to{" "}
+						{selectedTeam && selectedTeam.name}
+					</DialogDescription>
+				</DialogHeader>
+				{/* <form onSubmit={handleSubmit}>
+					<div>
+						<label>Email:</label>
+						<Input
+							type="text"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</div>
+					<div>
+						<label>Team ID:</label>
+						<Input
+							type="text"
+							value={teamId}
+							onChange={(e) => setTeamId(e.target.value)}
+						/>
+					</div>
+					<Button type="submit">Assign User to Team</Button>
+				</form> */}
+				<div className="flex flex-col gap-4 pt-3">
+					<div>
+						<Input
+							type="text"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</div>
+					<DialogFooter>
+						<Button
+							className="mt-4"
+							type="submit"
+							disabled={isLoading}
+							size="sm"
+							onClick={handleSubmit}
+						>
+							Add User
+						</Button>
+					</DialogFooter>
+				</div>
+			</DialogContent>
+		</Dialog>
 	);
 };
 
