7dd706d94dc7e88835286e7027902de35f7d2229
Joao Nina Matos <51080629+JoaoNM@users.noreply.github.com>
2024-07-06 02:51:28 +0100

    formatting firebase providers & replacing hard coded keys

--- components/firebase-providers.tsx
+++ components/firebase-providers.tsx
@@ -2,12 +2,12 @@
 
 import { FC, ReactNode, useMemo } from "react";
 import {
-  AnalyticsProvider,
-  AuthProvider,
-  FirebaseAppProvider,
-  FirestoreProvider,
-  useFirebaseApp,
-  DatabaseProvider,
+	AnalyticsProvider,
+	AuthProvider,
+	FirebaseAppProvider,
+	FirestoreProvider,
+	useFirebaseApp,
+	DatabaseProvider,
 } from "reactfire";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -17,55 +17,54 @@ import { FirebaseOptions } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const config: FirebaseOptions = {
-  apiKey: "AIzaSyAV3VTlHwmI9JEs68H-_GszFz7niKw2OV4",
-  authDomain: "syncspot-443de.firebaseapp.com",
-  databaseURL:
-    "https://syncspot-443de-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "syncspot-443de",
-  storageBucket: "syncspot-443de.appspot.com",
-  messagingSenderId: "1003511212608",
-  appId: "1:1003511212608:web:171a7f9b3a1134210acda5",
-  measurementId: "G-WX3GH107DN",
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const FirebaseProviderSDKs: FC<{ children: ReactNode }> = ({ children }) => {
-  const firebase = useFirebaseApp();
-  // we have to use getters to pass to providers, children should use hooks
-  const auth = useMemo(() => getAuth(), []);
-  const firestore = useMemo(() => getFirestore(firebase), []);
-  const database = useMemo(() => getDatabase(firebase), []);
-  const analytics = useMemo(() => isBrowser() && getAnalytics(firebase), []);
+	const firebase = useFirebaseApp();
+	// we have to use getters to pass to providers, children should use hooks
+	const auth = useMemo(() => getAuth(), []);
+	const firestore = useMemo(() => getFirestore(firebase), []);
+	const database = useMemo(() => getDatabase(firebase), []);
+	const analytics = useMemo(() => isBrowser() && getAnalytics(firebase), []);
 
-  return (
-    <>
-      {auth && (
-        <AuthProvider sdk={auth}>
-          <FirestoreProvider sdk={firestore}>
-            <DatabaseProvider sdk={database}>
-              {/* we can only use analytics in the browser */}
-              {analytics ? (
-                <AnalyticsProvider sdk={analytics}>
-                  {children}
-                </AnalyticsProvider>
-              ) : (
-                <>{children}</>
-              )}
-            </DatabaseProvider>
-          </FirestoreProvider>
-        </AuthProvider>
-      )}
-    </>
-  );
+	return (
+		<>
+			{auth && (
+				<AuthProvider sdk={auth}>
+					<FirestoreProvider sdk={firestore}>
+						<DatabaseProvider sdk={database}>
+							{/* we can only use analytics in the browser */}
+							{analytics ? (
+								<AnalyticsProvider sdk={analytics}>
+									{children}
+								</AnalyticsProvider>
+							) : (
+								<>{children}</>
+							)}
+						</DatabaseProvider>
+					</FirestoreProvider>
+				</AuthProvider>
+			)}
+		</>
+	);
 };
 
 export const MyFirebaseProvider: FC<{ children: ReactNode }> = ({
-  children,
+	children,
 }) => {
-  return (
-    <>
-      <FirebaseAppProvider firebaseConfig={config}>
-        <FirebaseProviderSDKs>{children}</FirebaseProviderSDKs>
-      </FirebaseAppProvider>
-    </>
-  );
+	return (
+		<>
+			<FirebaseAppProvider firebaseConfig={config}>
+				<FirebaseProviderSDKs>{children}</FirebaseProviderSDKs>
+			</FirebaseAppProvider>
+		</>
+	);
 };
