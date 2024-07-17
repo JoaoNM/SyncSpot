a717ddb00a601ad38eb58cb86a28e08392572aa8
Joao Nina Matos <51080629+JoaoNM@users.noreply.github.com>
2024-07-17 13:19:30 +0100

    add demo video

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -31,6 +31,11 @@ export default function Home() {
 									View Project on GitHub &rarr;
 								</Button>
 							</Link>
+							<iframe
+								src="https://www.loom.com/embed/b149cdacdbe54b078c3a1149cfc972d6?sid=5526cb7c-3141-4774-882f-b033752b1086"
+								allowFullScreen
+								className="w-[40vw] mt-0 rounded-xl h-[40vh] min-w-1/3 max-w-1/2"
+							></iframe>
 						</div>
 					</div>
 				</section>
